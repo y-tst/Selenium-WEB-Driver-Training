22a70272aae230d7a63bedd80eaef006282e6541
y-tst <yurii_yanchuk@epam.com>
2021-05-08 15:07:42 +0300

    WebDriver first commit

---
+++ src/main/java/com/epam/automation/CromeWebdriwerTraining.java
@@ -0,0 +1,4 @@
+package com.epam.automation;
+
+public class CromeWebdriwerTraining {
+}
---
+++ src/test/java/com/epam/automation/ChromeWebdriverTest.java
@@ -0,0 +1,13 @@
+package com.epam.automation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ChromeWebdriverTest {
+        @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
