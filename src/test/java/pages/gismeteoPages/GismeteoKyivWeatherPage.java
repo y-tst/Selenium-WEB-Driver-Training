8809de7b3a358aa25bf3da8634ca4a7c206e897a
y-tst <yurii_yanchuk@epam.com>
2021-06-09 15:59:55 +0300

    Redundant import removing

--- src/test/java/pages/gismeteoPages/GismeteoKyivWeatherPage.java
+++ src/test/java/pages/gismeteoPages/GismeteoKyivWeatherPage.java
@@ -1,6 +1,5 @@
 package pages.gismeteoPages;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
